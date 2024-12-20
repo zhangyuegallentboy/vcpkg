85e1a3c4294ca93fc864e2ba7285d03b474a9098
zhangyuegallentboy <asfraniv34@gmail.com>
2024-12-20 17:29:08 +0800

    Update x64-windows-static.cmake

--- triplets/x64-windows-static.cmake
+++ triplets/x64-windows-static.cmake
@@ -2,3 +2,16 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
+
+set(ICU_EXTRA_CONFIGURE_OPTIONS
+    "--with-data-packaging=static"
+    "--enable-static"
+    "--disable-shared"
+    "--enable-release"
+    "--disable-debug"
+    "--with-library-bits=64"
+    "--with-data-packaging=archive"
+    "CFLAGS=-DU_CHARSET_IS_UTF8=1"
+    "CXXFLAGS=-DU_CHARSET_IS_UTF8=1"
+    "--enable-locales=zh_CN"
+)
